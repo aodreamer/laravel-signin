65f7e388af5a9de903a8d2ec64bfe3609821902e
Rakha Maulana <Dreamwarior0@gmail.com>
2023-08-02 15:40:06 +0700

    upload sementara

--- resources/views/auth/certificate.blade.php
+++ resources/views/auth/certificate.blade.php
@@ -7,41 +7,26 @@
         <div class="alert alert-danger">{{ session('error') }}</div>
     @endif
     @if ($certificate)
-        <p>Nama Sertifikat: {{$certificate->p12_name}}</p>
-        <button class="btn btn-primary" onclick="showCertificate()">Download Sertifikat</button>
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <button class="btn btn-primary btn-lg btn-block" onclick="showCertificate()">Download Sertifikat</button>
+            </div>
+            <div class="col-md-4">
+                <button class="btn btn-primary btn-lg btn-block" onclick="showPasswordPopup()">Lihat Password P12</button>
+            </div>
+            <div class="col-md-4">
+                <form id="submitForm" method="POST" action="{{ route('cert.request') }}">
+                    @csrf
+                    <button type="button" class="btn btn-primary btn-lg btn-block" onclick="showSubmitRequestPopup()">Ajukan Perubahan</button>
+                </form>
+            </div>
+        </div>
     @else
         <p>Anda tidak memiliki sertifikat.</p>
     @endif
 </div>
 
 @if ($certificate)
-<div class="container mt-5">
-    <h2>Show P12 Password</h2>
-    @if(session('password'))
-    <div class="alert alert-success">
-        Pass Sertifikat: {{ session('password') }}
-    </div>
-    @endif
-    <button class="btn btn-primary" onclick="showPasswordPopup()">Lihat Password P12</button>
-</div>
-
-<div class="container mt-5">
-    <h2>Pengajuan Perubahan Sertifikat</h2>
-    @if(session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
-    </div>
-    @endif
-    <form method="POST" action="{{ route('cert.request') }}">
-        @csrf
-        <div class="form-group">
-            <label for="reason">Alasan Pengajuan</label>
-            <textarea class="form-control" id="reason" name="reason" rows="3" required></textarea>
-        </div>
-        <button type="button" class="btn btn-primary" onclick="submitRequest()">Ajukan Perubahan</button>
-    </form>
-</div>
-
 <div class="container mt-5">
     <h2>History Pengajuan</h2>
     @if($requests->isEmpty())
@@ -77,9 +62,10 @@
 <!-- JavaScript Code for SweetAlert2 Pop-ups -->
 <script>
   function showCertificate() {
+    const certificateName = "{{ $certificate->p12_name }}";
     Swal.fire({
       title: 'Download Sertifikat',
-      text: 'Are you sure you want to download the certificate?',
+      html: `Nama Sertifikat: ${certificateName}<br>Are you sure you want to download the certificate?`,
       icon: 'question',
       showCancelButton: true,
       confirmButtonText: 'Yes, download it!',
@@ -94,59 +80,82 @@
   }
 
   function showPasswordPopup() {
-  Swal.fire({
-    title: 'Enter Password',
-    html: '@csrf <input type="password" id="p12Password" class="swal2-input" required>',
-    confirmButtonText: 'Check',
-    showLoaderOnConfirm: true,
-    preConfirm: () => {
-      const password = document.getElementById('p12Password').value;
-      if(password==""){
-        return "Password kosong";
-      }
-      const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+    Swal.fire({
+      title: 'Enter Password',
+      html: '@csrf <input type="password" id="p12Password" class="swal2-input" required>',
+      confirmButtonText: 'Check',
+      showLoaderOnConfirm: true,
+      preConfirm: () => {
+        const password = document.getElementById('p12Password').value;
+        if (password == "") {
+          return "Password kosong";
+        }
+        const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
-      return fetch(`http://127.0.0.1:8000/showP12Password`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-CSRF-TOKEN': csrfToken,
-        },
-        body: JSON.stringify({ password: password, _token: csrfToken }),
-      })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error(response.statusText);
-          }
-          return response.text();
+        return fetch(`http://127.0.0.1:8000/showP12Password`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': csrfToken,
+          },
+          body: JSON.stringify({ password: password, _token: csrfToken }),
         })
-        .catch(error => {
-          Swal.showValidationMessage(`Request failed: ${error}`);
+          .then(response => {
+            if (!response.ok) {
+              throw new Error(response.statusText);
+            }
+            return response.text();
+          })
+          .catch(error => {
+            Swal.showValidationMessage(`Request failed: ${error}`);
+          });
+      },
+      allowOutsideClick: () => !Swal.isLoading(),
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: 'Pass Sertifikat',
+          text: result.value,
+          icon: 'success',
         });
-    },
-    allowOutsideClick: () => !Swal.isLoading(),
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire({
-        title: 'Pass Sertifikat',
-        text: result.value,
-        icon: 'success',
-      });
-    }
-  });
-}
+      }
+    });
+  }
 
-
-  function submitRequest() {
+  function showSubmitRequestPopup() {
     Swal.fire({
       title: 'Pengajuan Perubahan Sertifikat',
-      text: 'Are you sure you want to submit the request?',
-      icon: 'question',
+      html: '@csrf <form method="POST" action="{{ route('cert.request') }}">{{ csrf_field() }}<div class="form-group"><label for="reason">Alasan Pengajuan</label><textarea class="form-control" id="reason" name="reason" rows="3" required></textarea></div></form>',
       showCancelButton: true,
       confirmButtonText: 'Yes, submit it!',
       cancelButtonText: 'Cancel',
       confirmButtonColor: '#3085d6',
       cancelButtonColor: '#d33',
+      showLoaderOnConfirm: true,
+      preConfirm: () => {
+        const formData = new FormData();
+        formData.append('reason', document.getElementById('reason').value);
+        const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+        formData.append('_token', csrfToken);
+
+        return fetch(`http://127.0.0.1:8000/showP12Password`, {
+          method: 'POST',
+          headers: {
+            'X-CSRF-TOKEN': csrfToken,
+          },
+          body: formData,
+        })
+          .then(response => {
+            if (!response.ok) {
+              throw new Error(response.statusText);
+            }
+            return response.text();
+          })
+          .catch(error => {
+            Swal.showValidationMessage(`Request failed: ${error}`);
+          });
+      },
+      allowOutsideClick: () => !Swal.isLoading(),
     }).then((result) => {
       if (result.isConfirmed) {
         document.querySelector("form").submit();
