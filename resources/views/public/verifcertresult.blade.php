d7251b0e6e95e81e124a822cca14486d1184f9ca
Rakha Maulana <Dreamwarior0@gmail.com>
2023-08-03 14:23:13 +0700

    Update verifcertresult.blade.php

--- resources/views/public/verifcertresult.blade.php
+++ resources/views/public/verifcertresult.blade.php
@@ -1,13 +1,33 @@
-<!-- resources/views/result.blade.php -->
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>Result</title>
+    <!-- Include SweetAlert2 CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.min.css">
 </head>
 <body>
-    @if(isset($data))
-        <pre>{{ json_encode($data, JSON_PRETTY_PRINT) }}</pre>
+    @if(isset($data) && !empty($data))
+        <!-- Include SweetAlert2 JS -->
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.all.min.js"></script>
+        <script>
+            // Show the pop-up alert with JSON data
+            Swal.fire({
+=               html: '<pre>' + JSON.stringify(@json($data), null, 2) + '</pre>',
+                icon: 'info',
+                confirmButtonText: 'OK'
+            });
+        </script>
+    @else
+        <!-- Include SweetAlert2 JS -->
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.all.min.js"></script>
+        <script>
+            // Show the pop-up alert when there is no data
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Tidak ada sertifikat!',
+            });
+        </script>
     @endif
 </body>
 </html>
