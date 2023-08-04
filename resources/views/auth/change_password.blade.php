04c24c974b1c565f5774f4afc4c89d7d01643353
Rakha Maulana <Dreamwarior0@gmail.com>
2023-08-04 21:27:26 +0700

    Update change_password.blade.php

--- resources/views/auth/change_password.blade.php
+++ resources/views/auth/change_password.blade.php
@@ -1,61 +1,82 @@
-<!-- change_password.blade.php -->
-
 @extends('auth.dashboard')
 
 @section('content')
 <div class="container">
-    <h2>Profil Pengguna</h2>
-    <table class="table">
-        <tr>
-            <th>Nama</th>
-            <td>{{ $user->name }}</td>
-        </tr>
-        <tr>
-            <th>Email</th>
-            <td>{{ $user->email }}</td>
-        </tr>
-        <tr>
-            <th>Role</th>
-            <td>{{ $user->role }}</td>
-        </tr>
-    </table>
-</div>
-    <div class="container mt-5">
-        <h2>Change Password</h2>
-        @if(session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <h2 class="mb-0">Profile Information</h2>
+                </div>
+
+                <div class="card-body">
+                    <table class="table">
+                        <tr>
+                            <th>Name</th>
+                            <td>{{ $user->name }}</td>
+                        </tr>
+                        <tr>
+                            <th>Email</th>
+                            <td>{{ $user->email }}</td>
+                        </tr>
+                        <tr>
+                            <th>Role</th>
+                            <td>{{ $user->role }}</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
-        @endif
-
-        <form method="POST" action="{{ route('user.change-password') }}">
-            @csrf
-
-            <div class="form-group">
-                <label for="current_password">Current Password</label>
-                <input type="password" name="current_password" id="current_password" class="form-control @error('current_password') is-invalid @enderror">
-                @error('current_password')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-            </div>
-
-            <div class="form-group">
-                <label for="new_password">New Password</label>
-                <input type="password" name="new_password" id="new_password" class="form-control @error('new_password') is-invalid @enderror">
-                @error('new_password')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-            </div>
-
-            <div class="form-group">
-                <label for="new_password_confirmation">Confirm New Password</label>
-                <input type="password" name="new_password_confirmation" id="new_password_confirmation" class="form-control @error('new_password_confirmation') is-invalid @enderror">
-                @error('new_password_confirmation')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-            </div>
-
-            <button type="submit" class="btn btn-primary">Change Password</button>
-        </form>
+        </div>
     </div>
+</div>
+
+<div class="container mt-4">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    <h2 class="mb-0">Change Password</h2>
+                </div>
+
+                <div class="card-body">
+                    @if(session('success'))
+                        <div class="alert alert-success">
+                            {{ session('success') }}
+                        </div>
+                    @endif
+
+                    <form method="POST" action="{{ route('user.change-password') }}">
+                        @csrf
+
+                        <div class="form-group">
+                            <label for="current_password">Current Password</label>
+                            <input type="password" name="current_password" id="current_password" class="form-control @error('current_password') is-invalid @enderror">
+                            @error('current_password')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <label for="new_password">New Password</label>
+                            <input type="password" name="new_password" id="new_password" class="form-control @error('new_password') is-invalid @enderror">
+                            @error('new_password')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                        <div class="form-group">
+                            <label for="new_password_confirmation">Confirm New Password</label>
+                            <input type="password" name="new_password_confirmation" id="new_password_confirmation" class="form-control @error('new_password_confirmation') is-invalid @enderror">
+                            @error('new_password_confirmation')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Change Password</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
